c847e670f7b4aaff9dd5f0295464d0f13ca84965
Ankit Pant <159069425+Ankit-0712@users.noreply.github.com>
2025-08-23 12:30:31 +0530

    Time: 23 ms (10.96%) | Memory: 42.3 MB (85.94%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,17 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        //Check for length of both string
         if(s.length() != t.length()) return false;
 
+        //use two maps to store the values from both string
         Map<Character, Character> mapST = new HashMap<>();
         Map<Character, Character> mapTS = new HashMap<>();
 
+        //iterarte over the both strings together
         for(int i=0;i<s.length();i++){
-            char c1 = s.charAt(i);
+            char c1 = s.charAt(i);//initialize 2 char to store first value of both str
             char c2 = t.charAt(i);
 
+            //check if char present in the map and it c1!=c2
             if(mapST.containsKey(c1)&& mapST.get(c1)!=c2) return false;
             if(mapTS.containsKey(c2) && mapTS.get(c2) != c1) return false;
 
+            //if abpve condotion false put c1, c2 in maps
             mapST.put(c1,c2);
             mapTS.put(c2,c1);
         }

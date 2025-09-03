6bb7ea182f87bb5fa41b7d1edc4b198719716070
Ankit Pant <159069425+Ankit-0712@users.noreply.github.com>
2025-09-03 13:06:32 +0530

    Time: 2782 ms (5.80%) | Memory: 45.4 MB (43.48%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+        int count= 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+
+                int ax= points[i][0];
+                int ay = points[i][1];
+                int bx = points[j][0];
+                int by = points[j][1];
+
+                if(ax<=bx && ay>= by){
+                    boolean valid = true;
+
+                    for(int k = 0;k<n;k++){
+                        if(k==i || k==j) continue;
+
+                        int cx = points[k][0];
+                        int cy = points[k][1];
+
+                        if(ax<=cx && cx<=bx && by<=cy && cy<= ay){
+                            valid = false;
+                            break;
+                        }
+                    }
+
+                    if(valid) count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}

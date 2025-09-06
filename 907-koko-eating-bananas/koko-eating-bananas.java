e1e9277ea41a63c817a13c625ecabc1e85639235
Ankit Pant <159069425+Ankit-0712@users.noreply.github.com>
2025-09-06 13:10:36 +0530

    Time: 25 ms (35.36%) | Memory: 45.2 MB (46.38%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(piles[i]>maxi){
+                maxi = piles[i];
+            }
+        }
+
+        int start = 1, end = maxi;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(sumD(piles,mid) <= h){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return start;
+    }
+
+    private int sumD(int[] nums, int div){
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += Math.ceil((double)nums[i]/(double)div);
+        }
+
+        return sum;
+    }
+}

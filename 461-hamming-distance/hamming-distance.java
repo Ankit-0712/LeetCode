327bd7fb1bffc288b04eccaba31801560420d5a7
Ankit Pant <159069425+Ankit-0712@users.noreply.github.com>
2026-01-14 20:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (78.93%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int num = x ^ y;
+        int count = 0;
+        for(int i=0;i<32;i++){
+            count += (num & 1);
+            num = num >> 1;
+
+        }
+
+        return count;
+    }
+}
